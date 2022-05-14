77878c062ac5b4ec867dc4cc316f038faee965bc
t56k <txnash@gmail.com>
2022-05-14 16:00:36 +1000

    had the wrong args

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 git add .
-git commit -m $0
+git commit -m "$1"
 git push origin main
 
 cp public/* ../t56k.github.io
 cd ../t56k.github.io
 git add .
-git commit -m $0
+git commit -m "$1"
 git push origin main
 
 cd ../ssrg
